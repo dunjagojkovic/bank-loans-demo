28a80c6770e8dc41e1543937b202ab848cc5ac0d
Dunja Gojkovic <gojkovic.ra37.2018@uns.ac.rs>
2024-10-14 17:00:37 +0200

    refactor creating bank loan type

--- demo/src/main/kotlin/com/example/demo/dao/bankloantype/BankLoanTypeDaoImpl.kt
+++ demo/src/main/kotlin/com/example/demo/dao/bankloantype/BankLoanTypeDaoImpl.kt
@@ -4,16 +4,18 @@ import com.example.demo.exception.specific.BankLoanTypeNameAlreadyExists
 import com.example.demo.model.bankloantype.BankLoanType
 import com.example.demo.repository.bankloantype.BankLoanTypeRepository
 import org.springframework.stereotype.Service
+import kotlin.jvm.optionals.getOrNull
 
 @Service
 class BankLoanTypeDaoImpl(
     private val bankLoanTypeRepository: BankLoanTypeRepository
 ): BankLoanTypeDao {
     override fun create(bankLoanType: BankLoanType): BankLoanType {
-        if(bankLoanTypeRepository.findByName(bankLoanType.name).isPresent){
-            throw BankLoanTypeNameAlreadyExists(bankLoanType.name)
-        }
-        return bankLoanTypeRepository.save(bankLoanType)
+        bankLoanTypeRepository.findByName(bankLoanType.name).getOrNull()
+            ?.let {
+                throw BankLoanTypeNameAlreadyExists(bankLoanType.name)
+            }
+       return bankLoanTypeRepository.save(bankLoanType)
     }
 
 }
